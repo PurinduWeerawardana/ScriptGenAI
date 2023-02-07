fdaad3828b9c6ec22a92e5889e653697419bcd9d
Thamash <33304623+PurinduWeerawardana@users.noreply.github.com>
2023-02-07 09:55:53 +0530

    Create route for Generate Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import Upload from "./pages/Upload";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Generate from "./pages/Generate";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <Route path="/upload" element={<Upload />}></Route>
       <Route path="/login" element={<Login />}></Route>
       <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/generate" element={<Generate />}></Route>
+      <Route path="/*" element={<h1>404 not found Squadrons</h1>}></Route>
     </Routes>
   );
 }
