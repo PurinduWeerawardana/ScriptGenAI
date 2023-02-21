e48585ff153ec300810ff024dec0a34faa656cca
Rajani Sooriyaarachchi <95573031+RajaniSooriya@users.noreply.github.com>
2023-02-21 20:53:46 +0530

    import from react icons

---
+++ src/icons-react/Facebook.js
@@ -0,0 +1,13 @@
+import FcFacebook from 'react-icons/fc'
+
+import React from 'react'
+
+const Facebook = () => {
+  return (
+    <div>
+      <Facebook/>
+    </div>
+  )
+}
+
+export default Facebook
---
+++ src/icons-react/Google.js
@@ -0,0 +1,13 @@
+import FcGoogle from 'react-icons/fc';
+
+import React from 'react'
+
+const GoogleIcon = () => {
+  return (
+    <div>
+      <FcGoogle/>
+    </div>
+  )
+}
+
+export default GoogleIcon
