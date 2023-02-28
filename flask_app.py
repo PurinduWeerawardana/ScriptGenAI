8431a2a7efd1a9bd682e227cf5f9d8c233b13c7a
Thamash <33304623+PurinduWeerawardana@users.noreply.github.com>
2023-02-28 20:23:09 +0530

    Connect with openAi

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,30 @@
 from flask import Flask
+import re
+import openai
+
+# set API key
+openai.api_key = "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"
+
+def generateScripts(slides):
+    print("Completing...")
+    # list for save generated scripts
+    generatedScripts = []
+    for slide in slides:
+        # create the prompt, substring with regex for remove S1: S2: notation
+        promptForGPT = "Write a presentation script->\\n" + \
+            re.sub('S\d:', '', slide)
+        # call openai for completion
+        completion = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=promptForGPT,
+            temperature=0.7,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            max_tokens=200)
+        # append to generated scripts list
+        generatedScripts.append(completion.choices[0].text)
+    return generatedScripts
 
 app = Flask(__name__)
 
