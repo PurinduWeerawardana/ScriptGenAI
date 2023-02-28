3fc675eb76acf97cb0b6e6f50338f17c88b31317
Thamash <33304623+PurinduWeerawardana@users.noreply.github.com>
2023-02-28 19:14:58 +0530

    Create slide preview

---
+++ public/webviewer/lib/core/WebDBPartRetriever.chunk.js
@@ -0,0 +1,3 @@
+/** Notice * This file contains works from many authors under various (but compatible) licenses. Please see core.txt for more information. **/
+(function(){(window.wpCoreControlsBundle=window.wpCoreControlsBundle||[]).push([[13],{469:function(ia,ca,e){e.r(ca);var ea=e(0),fa=e(1);e.n(fa);ia=e(100);e=e(395);ia=function(e){function ba(aa,y,x){y=e.call(this,aa,y,x)||this;y.db=aa;return y}Object(ea.c)(ba,e);ba.prototype.request=function(e){var y=this;Object(fa.each)(e,function(e){y.db.get(e,function(w,r,h){return w?y.trigger("partReady",{lb:e,error:w}):y.trigger("partReady",{lb:e,data:r,$i:!1,yg:!1,error:null,md:h})})})};ba.prototype.lu=function(e){e()};
+return ba}(ia.a);Object(e.a)(ia);Object(e.b)(ia);ca["default"]=ia}}]);}).call(this || window)
