c26651b71886847cd518151393b64e374cca13d3
Jaiyramanan <103107389+jaiyramanan23@users.noreply.github.com>
2023-03-26 14:23:58 +0530

    File uploaded Upload button Lock

--- src/components/UploadCard.jsx
+++ src/components/UploadCard.jsx
@@ -1,18 +1,10 @@
 import { useState } from "react";
-import {
-  Card,
-  Button,
-  Progress,
-  input
-} from "@material-tailwind/react";
+import { Card, Button, Progress, input } from "@material-tailwind/react";
 import uploadIcon from "../static/images/upload-icon.png";
-import {
-  ref,
-  uploadBytesResumable,
-  getDownloadURL
-} from "firebase/storage";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
 
 const app = initializeApp({
   apiKey: "AIzaSyAs3dSZySiNa5yCY2MSqvmCVKexMTSxQ3E",
@@ -21,34 +13,42 @@ const app = initializeApp({
   storageBucket: "sdgp-squadr.appspot.com",
   messagingSenderId: "411601539731",
   appId: "1:411601539731:web:8f9e7ca228e25575663366",
-  measurementId: "G-N48LSP4X4J"
+  measurementId: "G-N48LSP4X4J",
 });
 
 const storage = getStorage(app);
 
 function App() {
+  const navigate = useNavigate();
   // State to store uploaded file
   const [file, setFile] = useState("");
 
   // progress
   const [percent, setPercent] = useState(0);
 
+  // uploading state
+  const [isUploading, setIsUploading] = useState(false);
+
   // Handle file upload event and update state
   function handleChange(event) {
     const selectedFile = event.target.files[0];
-    if (selectedFile.type !== "application/vnd.openxmlformats-officedocument.presentationml.presentation") {
+    if (
+      selectedFile.type !==
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+    ) {
       alert("Please select a .pptx file!");
       setFile("");
     } else {
       setFile(selectedFile);
     }
   }
-  
 
   const handleUpload = () => {
     if (!file) {
       alert("Please upload the Presentation Slides!");
     }
+
+    setIsUploading(true); // Set isUploading to true when the upload starts
 
     const storageRef = ref(storage, `/files/${file.name}`);
 
@@ -71,6 +71,9 @@ function App() {
         // download url
         getDownloadURL(uploadTask.snapshot.ref).then((url) => {
           console.log(url);
+          console.log(file.name);
+          navigate("/generate", { state: { link: url, fileName: file.name } });
+          setIsUploading(false); // Set isUploading to false when the upload is complete
         });
       }
     );
@@ -78,10 +81,7 @@ function App() {
 
   return (
     <div>
-      
-
       <Card className="w-9/12 h-96 m-auto">
-        
         <Card className="bg-lightPurple m-6 h-full">
           <p>{percent} % </p>
 
@@ -93,12 +93,20 @@ function App() {
             className="w-full h-9 mb-4 bg-gray-200 rounded-full dark:bg-gray-700 shadow-xl "
           />
 
-          <img src={uploadIcon} alt="" className="w-24 m-auto" onClick={input} />
+          <img
+            src={uploadIcon}
+            alt=""
+            className="w-24 m-auto"
+            onClick={input}
+          />
           <div className="text-center">
-          <label htmlFor="pptx-file" className="block text-gray-700 font-bold mb-2">
-            Please select a PPTX file:
-          </label>
-        
+            <label
+              htmlFor="pptx-file"
+              className="block text-gray-700 font-bold mb-2"
+              >
+                Please select a PPTX file:
+              </label>
+  
               <input
                 type="file"
                 id="pptx-file"
@@ -108,7 +116,6 @@ function App() {
                 style={{
                   backgroundColor: "#F3F4F6",
                   color: "#6B7280",
-                
                   padding: "15px 70px",
                   boxShadow: "none",
                   border: "none",
@@ -116,24 +123,24 @@ function App() {
                   cursor: "pointer",
                 }}
               />
-          </div>
-
-          <Button
-            onClick={handleUpload}
-            variant="gradient"
-            color="deep-purple"
-            size="md"
-            className="mb-6 mx-10 rounded-full"
-          >
-            
-            <span>Upload</span>
-          </Button>
-          
+            </div>
+  
+            <Button
+              onClick={handleUpload}
+              variant="gradient"
+              color="deep-purple"
+              size="md"
+              className="mb-6 mx-10 rounded-full"
+              disabled={isUploading} // Disable the button if isUploading is true
+            >
+              <span>Upload</span>
+            </Button>
+          </Card>
         </Card>
-      </Card>
-      
-    </div>
-  );
-}
+      </div>
+    );
+  }
+  
+  export default App;
+  
 
-export default App;
