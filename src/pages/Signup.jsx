05a35ef4e15ce888b52d5f9fc71d059f6d4f2d6c
SHIWARNE_SILVA <sshiwarne@gmail.com>
2023-03-27 06:23:48 +0530

    made signup responsive and button changes

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,15 +2,17 @@ import Navbar from "../components/NavBar";
 import { Button, Input } from "@material-tailwind/react";
 import illustration from "../static/images/login-illustration.png";
 import blob from "../static/images/blob.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
+  const navigate = useNavigate();
   return (
     <div className="upload">
       <Navbar></Navbar>
       <div className="blob">
         <img src={blob} alt="" />
       </div>
-      <div className="content grid gap-4 grid-cols-2 w-10/12 my-32 mx-auto">
+      <div className="content grid gap-4 grid-cols-1 sm:grid-cols-2 w-10/12 my-32 mx-auto">
         <div className="left my-auto">
           <img src={illustration} alt="" className="w-9/12 m-auto" />
         </div>
@@ -18,7 +20,7 @@ export default function Signup() {
           <h1 className="text-left text-4xl sm:text-4xl font-sans font-semibold mb-4">
             Sign<span className="text-textPurple"> Up</span>
           </h1>
-          <div className="flex w-10/12 flex-col gap-6">
+          <div className="flex flex-col gap-6">
             <Input size="lg" color="indigo" label="Name" />
             <Input size="lg" color="indigo" label="Email Address" />
             <Input size="lg" color="indigo" label="Password" />
@@ -26,9 +28,9 @@ export default function Signup() {
             <Button color="deep-purple" size="lg" block>
               Sign Up
             </Button>
-            <div id="social-login">
+            <div id="social-login" className="flex flex-col gap-2 sm:flex-row sm:justify-around sm:items-center">
               <p className="text-center text-sm">Or log in with</p>
-              <div className="flex flex-row-3 justify-around">
+              <div className="flex gap-4 justify-around">
                 <Button
                   color="white"
                   size="lg"
@@ -73,9 +75,33 @@ export default function Signup() {
               </div>
             </div>
             <div className="flex flex-row-3 justify-between text-textPurple">
+            <Button
+                variant="gradient"
+                color="deep-purple"
+                size="md"
+                className="bg-indigo-900 text-white font-bold py-4 px-6 m-0 rounded-full"
+                onClick={() => navigate("/privacypolicy")}
+              >
               <p>Terms</p>
+              </Button>
+              <Button
+                variant="gradient"
+                color="deep-purple"
+                size="md"
+                className="bg-indigo-900 text-white font-bold py-4 px-6 m-0 rounded-full"
+                onClick={() => navigate("/privacypolicy")}
+              >
               <p>Privacy</p>
+              </Button>
+              <Button
+                variant="gradient"
+                color="deep-purple"
+                size="md"
+                className="bg-indigo-900 text-white font-bold py-4 px-6 m-0 rounded-full"
+                onClick={() => navigate("/privacypolicy")}
+              >
               <p>Security</p>
+              </Button>
             </div>
           </div>
         </div>
