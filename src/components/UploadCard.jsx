8ba3b918761e42f4d7382efa8fce6a12208729fc
SHIWARNE_SILVA <sshiwarne@gmail.com>
2023-03-31 10:42:00 +0530

    made upload page responsive

--- src/components/UploadCard.jsx
+++ src/components/UploadCard.jsx
@@ -109,7 +109,7 @@ function App() {
                   backgroundColor: "#F3F4F6",
                   color: "#6B7280",
                 
-                  padding: "15px 70px",
+                  padding: "5px 1px",
                   boxShadow: "none",
                   border: "none",
                   outline: "none",
