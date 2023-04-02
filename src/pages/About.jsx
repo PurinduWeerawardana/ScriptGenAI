23902e33290a35ee51e5283ec7d30cf9be8cf82a
SHIWARNE_SILVA <sshiwarne@gmail.com>
2023-04-02 22:27:29 +0530

    made changes in team members

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -35,7 +35,7 @@ export default function Home() {
       <div class="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
 
           <div class="items-center bg-indigo-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
+              <a href="About">
                   <img class=" w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="Images/Rajani1.png" alt="Rajani"></img>
               </a>
               <div class="p-5">
@@ -72,7 +72,7 @@ export default function Home() {
 
 
           <div class="items-center bg-indigo-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
+              <a href="About">
                   <img class=" w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="Images/Purindu1.png" alt="Purindu"></img>
               </a>
               <div class="p-5">
@@ -109,7 +109,7 @@ export default function Home() {
 
 
           <div class="items-center bg-indigo-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
+              <a href="About">
                   <img class=" w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="Images/Lakshan1.png" alt="Lakshan"></img>
               </a>
               <div class="p-5">
@@ -136,7 +136,7 @@ export default function Home() {
                       </li>
                       <li>
                           
-                          <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                          <a href="about" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
                               <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" /></svg>
                           </a>
                       </li>
@@ -145,7 +145,7 @@ export default function Home() {
           </div> 
           
           <div class="items-center bg-indigo-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
+              <a href="About">
                   <img class="w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="Images/Shiwarne2.png" alt="Shiwarne"></img>
               </a>
               <div class="p-5">
@@ -182,7 +182,7 @@ export default function Home() {
 
 
           <div class="items-center bg-indigo-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-              <a href="#">
+              <a href="About">
                   <img class=" w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="Images/Jairamanan1.png" alt="Jairamanan"></img>
               </a>
               <div class="p-5">
